b84a755ca4c3e3c97557a0e499729f750f506354
dengxuan <303359166@qq.com>
2021-02-20 09:32:26 +0800

    新增 逻辑状态机 基本框架

---
+++ script/StateMachine/StateMachinePlayer.gd
@@ -0,0 +1,16 @@
+extends Node
+class_name StateMachinePlayer
+
+export(Resource) var state_machine
+
+func _physics_process(delta):
+	pass
+	
+func _process(delta):
+	pass
+	
+func _unhandled_input(event):
+	pass
+	
+func _input(event):
+	pass
